bfb43a1213f70824129f797c07b701a7cd2d2bc2
taryn yaeger <tsyaeger@gmail.com>
2018-05-20 01:08:04 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+
+
+class School
+  
+  attr_accessor :student, :grade, :school_name, :roster, :student_name
+
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  
+  def sort 
+    @roster.sort 
+  end
+
+  
+  def add_student=(student, grade)
+    @student_name = student
+    if @roster.include?(grade) == true
+      @roster[grade] << student
+    else 
+      @roster[grade] = []
+      
+    end
+  end
+  
+end
+    
+    
+    
+    
+    
+    
