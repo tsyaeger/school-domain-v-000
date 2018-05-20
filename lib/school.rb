0dbdf47efdc36ab47cff68cbab42e11f0d3bd503
taryn yaeger <tsyaeger@gmail.com>
2018-05-20 01:37:05 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,41 +1,34 @@
 # code here!
 
-
 class School
-  
 
-  
-  def initialize(school_name)
-    @school_name = school_name
+  def initialize(school)
+    @school_name = school
     @roster = {}
   end
-  
-  
+
   def roster
     @roster
   end
-  
-    
-  def add_student(student, grade)
-    @student_name = student
+
+  def add_student(student_name, grade)
+    @student_name = student_name
     @grade = grade
     if @roster.include?(grade) == false
-      @roster[grade] == []
+      @roster[grade] = []
     end
-      @roster[grade] << (student)
-  end
-  
-  
-  def sort
-    @roster.each do |grade,student|
-      @roster[grade]
-    end
-  end
-  
-  def grade(grade)
-    @roster[grade]
+    @roster[grade] << name
   end
 
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
 end
     
     
